c5e63e390e3581e56a22cf3db102f43096ac7bb3
Samuel Mohler <smohler@pdx.edu>
2020-04-13 15:57:21 -0700

    added some magic method and pseudo code

--- code/pypipe.py
+++ code/pypipe.py
@@ -11,6 +11,7 @@ class pypipe:
     version = '0.0.0'
     #class constants
     g = 9.81 #[m/s^2]
+    units = 'SI'
 
     #fluid properties
 
@@ -37,8 +38,27 @@ class pypipe:
         self.rho = rho[fluid]
         self.mu = mu[fluid]
 
+
     #class methods
+
+    def getPipeSchedule(self):
+        #search diameter pipe which best fits manufactured pipe diameters
+        pass
+
+    
 
     #get head loss
 
     #magic methods __add__, __main__, __len__, etc methods
+
+    def __repr__(self):
+        return {'Diameter': self.D, 'Length': self.L, 'Material': self.e, 'Fluid': (self.rho, self.mu)}
+
+    def __len__(self):
+        #when calling len() in python on the pipe return the length
+        return self.L
+
+    def __str__(self):
+        #TODO check unit system to display
+        txt = "Diameter:{0}[{unit}]__Length:{1}[{unit}]__Roughness:{2}[{unit}]"
+        return txt.format(self.D, self.L, self.e, unit = self.unit)
