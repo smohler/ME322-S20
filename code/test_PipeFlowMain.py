11f6da9cbc73720b6cd0a463cedc0f1f89b7783a
Samuel Mohler <smohler@pdx.edu>
2020-04-15 22:39:32 -0700

    added test file pseudo code/learning example

---
+++ code/test_PipeFlowMain.py
@@ -0,0 +1,28 @@
+#Test file for functions in PipeFlowMain.py
+#unit testing is very important to make sure minor increments work
+#there is a python package unittest you can use or write your own unit test.
+import PipeFlowMain as pf
+
+
+#Each function in PipeFlowMain.py needs to be tested with known input and output.
+#Here is how I write unittests. 
+# test_functionname_testnumber = file.functionname(known inputs) == known output
+
+test_getFrictionFactor_1 = pf.getFrictionFactor('Known Inputs') == 'Known Output' 
+
+#Then its helpful to print a statement if it passes or if it fails the output
+def test_results_print(result, func, output, inputs):
+    """ Printer function for test results.
+        results := Boolean 
+        func    := Function Object
+        output  := Function Output
+        input   := (arg1, arg2, ... argN) tuple
+    """
+    if result is True:
+        print('Test {} PASSED'.format(func.__name__))
+    else:
+        print('Test {} FAILED: Output:{} not {}'.format(func.__name__, output, func(*inputs)))
+
+test_results_print(test_getFrictionFactor_1, getFrictionFactor, 'True Answer', 'used inputs')
+
+
